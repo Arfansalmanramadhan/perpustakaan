62f8647a6b08d06d0079fcd477ca08fd0bb681f5
Arfansalmanramadhan <arfansalman9@gmail.com>
2023-06-03 11:41:45 +0700

    memmbua layout template responsive

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Profile</title>
-</head>
-<body>
-    Profile
-</body>
-</html>
+@extends('layout.mainlayout')
+
+@section('title', "Profile")
+    
+@section('content')
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, dolorum. Atque, pariatur cupiditate optio cum, facere magni repellat quaerat quod, est possimus a voluptatem error mollitia sed aperiam dignissimos unde!    
+    </p>
+@endsection
