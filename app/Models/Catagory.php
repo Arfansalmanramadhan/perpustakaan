e5cae51854db9923e3d9705162fc2931ec57c1cb
Arfansalmanramadhan <arfansalman9@gmail.com>
2023-06-06 19:51:14 +0700

    membuat slug

--- app/Models/Catagory.php
+++ app/Models/Catagory.php
@@ -3,13 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Catagory extends Model
 {
     use HasFactory;
-
+    use Sluggable;
     protected $fillable = [
-        "name"
+        "name",
+        "slug"
     ];
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 }
