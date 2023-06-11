6b59ed26002031013adc8c44921ac755699bf26f
Arfansalmanramadhan <arfansalman9@gmail.com>
2023-06-11 23:55:47 +0700

    selesai pencarian

--- resources/views/daftar-buku.blade.php
+++ resources/views/daftar-buku.blade.php
@@ -4,12 +4,30 @@
     
 @section('content')
 
+    <form action="" method="get">
+    <div class="row">
+        <div class="col-12 col-sm-6">
+            <select name="catagori" id="catagori" class="form-control">
+                <option value="">Pilih Kategori</option>
+                @foreach ($kategori as $item)
+                    <option value="{{$item->id}}">{{$item->name}}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="col-12 col-sm-6">
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Cari judul buku " aria-label="Recipient's username" aria-describedby="basic-addon2" name="title">
+                <button type="submit" class="btn btn-primary">Cari</button>
+            </div>
+        </div>
+    </div>
+    </form>
     <div class="container">
         <div class="row ">
             @foreach ($buku as $item)
                 
                 <div class="col-lg-3 col-md-4 col-sm-6 md-3">
-                    <div class="card" >
+                    <div class="card h-100" >
                         <img src="{{asset('storage/cover/'.$item->cover)}}" class="card-img-top" draggable="false">
                         <div class="card-body">
                             <h5 class="card-title">{{$item->book_code}}</h5>
