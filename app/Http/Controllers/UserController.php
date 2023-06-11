81ff058b326a1e6bf608989fc1dfd25ea6f6a2ef
Arfansalmanramadhan <arfansalman9@gmail.com>
2023-06-11 12:39:58 +0700

    membuat tampilanuser

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -13,6 +14,7 @@ class UserController extends Controller
     }
     public function user()
     {
-        return view("user");
+        $user = User::where("role_id", 2)->get();
+        return view("user", ["user" => $user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,4 +4,38 @@
     
 @section('content')
     <h1>user</h1>
+    <div class="mt-5 d-flex justify-content-end">
+        <a href="dihapus-kategori" class="btn btn-secondary me-3">Lihat melarang pengguna</a>
+        <a href="tambah-kategori" class="btn btn-primary">Lihat register baru</a>
+    </div>
+    <div class="my-5">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>No Telepon</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($user as $item)
+                    <tr>
+                        <td>{{$loop->iteration}}</td>
+                        <td>{{$item->username}}</td>
+                        <td>
+                            @if ($item->phone)
+                                {{$item->phone}}
+                            @else
+                                -
+                            @endif
+                        </td>
+                        <td>
+                            <a href="/edit-buku/{{$item->slug}}">Edit</a>
+                            <a href="/hapus-buku/{{$item->slug}}">Melarang Perngguna</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
